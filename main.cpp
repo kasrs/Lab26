2b859cb7c6fd3e8172bddcd3b2553a352d31cc05
Zac <32443586+ZacCampos@users.noreply.github.com>
2018-04-29 22:14:46 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * AUTHORS    : Zac Campos & Kasra Tabib
+ * LAB #26    : Enumerated Types
+ * CLASS      : CS1A
+ * SECTION    : MW - 8am
+ * DUE DATE   : 4/30/18
+ ******************************************************************************/
+
+#include "header.h"
+
+int main()
+{
+	Season currentSeason;
+	char season;
+
+	season = GetSeason();
+
+	currentSeason = ConvertCharToSeason(currentSeason);
+
+
+
+
+	return 0;
+}
