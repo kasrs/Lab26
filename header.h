5a15c602e806e7e89ae6badf59bde8b3edfc5778
kasrs <37860723+kasrs@users.noreply.github.com>
2018-04-30 05:13:27 -0700

    Update header.h

--- header.h
+++ header.h
@@ -7,11 +7,13 @@
 
 using namespace std;
 
+//seasons array
 string Seasons[] =
 {
 		"Winter", "Spring", "Summer", "Fall"
 };
 
+//fruits array
 string Fruits[] =
 {
 		"Kishu Mini Mandarin",
@@ -20,6 +22,7 @@ string Fruits[] =
 		"Star Ruby Red Grapefruit"
 };
 
+//enumerated type seasons
 enum Season
 {
 	WINTER,
@@ -28,12 +31,42 @@ enum Season
 	FALL
 };
 
+/******************************************************************************
+* GetSeason
+*	This function provides a menu to the user with their choices of
+*	different seasons and prompts them to type in a specific season. This
+*	function can handle both upper and lower case letters, and it will return
+*	a character that represents one of the seasons.
+*
+* => returns season
+******************************************************************************/
 char GetSeason();
 
+/******************************************************************************
+ * ConvertCharToSeason
+ * 	This function converts a character to the enumerated type season and
+ * 	returns a variable of type seasons.
+ *
+ * => returns season
+ *****************************************************************************/
 Season ConvertCharToSeason(char season);
 
+/******************************************************************************
+ * ConvertSeasonToString
+ * 	This function converts the enumerated type season to a string representing
+ * 	the season and returns a variable of type string.
+ *
+ * => returns seasons
+ *****************************************************************************/
 string ConvertSeasonToString(Season currentSeason);
 
+/******************************************************************************
+ * ConvertSeasonToFruit
+ * 	This function converts the enumerated type seasons to a string representing
+ * 	the fruit, and returns a variable of type string.
+ *
+ * => returns fruits
+ *****************************************************************************/
 string ConvertSeasontoFruit(Season currentSeason);
 
 
