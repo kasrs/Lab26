6b5c27d23b82a2ecdb37af2bb0fda2e88f99c938
Zac <32443586+ZacCampos@users.noreply.github.com>
2018-04-29 22:15:56 -0700

    Create ConvertSeasonToString.cpp

---
+++ ConvertSeasonToString.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+ * AUTHORS    : Zac Campos & Kasra Tabib
+ * LAB #26    : Enumerated Types
+ * CLASS      : CS1A
+ * SECTION    : MW - 8am
+ * DUE DATE   : 4/30/18
+ ******************************************************************************/
+
+#include "header.h"
+
+string ConvertSeasonToString(Season currentSeason)
+{
+	return Seasons[currentSeason];
+}
