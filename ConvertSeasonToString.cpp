166638caad04e1f03d7933712c9a74ea78331818
kasrs <37860723+kasrs@users.noreply.github.com>
2018-04-30 05:12:10 -0700

    Update ConvertSeasonToString.cpp

--- ConvertSeasonToString.cpp
+++ ConvertSeasonToString.cpp
@@ -8,7 +8,24 @@
 
 #include "header.h"
 
+/******************************************************************************
+ * FUNCTION - ConvertSeasonToString
+ * ----------------------------------------------------------------------------
+ * 	This function converts the enumerated type season to a string representing
+ * 	the season and returns a variable of type string.
+ *
+ * => returns seasons
+ * ----------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ * 	the following need a pre-defined value passed in
+ * 		currentSeason
+ *
+ * POST-CONDITIONS
+ * 	the function returns the season in string form
+ *****************************************************************************/
 string ConvertSeasonToString(Season currentSeason)
 {
+	//converting the character that represents a season to the full length season
+	//name
 	return Seasons[currentSeason];
 }
